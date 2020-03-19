5535073a591dc830fac049e229e8a3ac0da7759e
CalebLehman <lehman.346@osu.edu>
2020-03-18 22:26:36 -0400

    Retab

--- include/defs.h
+++ include/defs.h
@@ -11,50 +11,50 @@ typedef struct EdgeList EdgeList;
 typedef struct PointList PointList;
 
 /* By convention:
- * 	Positive rotation about a point
- * 	is counter-clockwise
- * 	It follows that positive rotation
- * 	about exterior of polygonal boundary
- * 	is clockwise and about interior is
- * 	counter-clockwise
+ *  Positive rotation about a point
+ *  is counter-clockwise
+ *  It follows that positive rotation
+ *  about exterior of polygonal boundary
+ *  is clockwise and about interior is
+ *  counter-clockwise
  */
 struct Point
 {
-	VALUE x;
-	VALUE y;
-	Edge *e;
+    VALUE x;
+    VALUE y;
+    Edge *e;
 };
 
 struct Edge
 {
-	Point *orig;
-	Edge *oprev;
-	Edge *dnext;
-	Edge *twin;
+    Point *orig;
+    Edge *oprev;
+    Edge *dnext;
+    Edge *twin;
 };
 
 struct ExtremeEdge
 {
-	Edge *left_edge_ccw;
-	Edge *right_edge_cw;
-	Edge *bottom_edge_ccw;
-	Edge *top_edge_cw;
+    Edge *left_edge_ccw;
+    Edge *right_edge_cw;
+    Edge *bottom_edge_ccw;
+    Edge *top_edge_cw;
 };
 
 struct PointList
 {
-	Point *points;
-	Point **unused_points;
-	size_t idx;
-	size_t size;
+    Point *points;
+    Point **unused_points;
+    size_t idx;
+    size_t size;
 };
 
 struct EdgeList
 {
-	Edge *edges;
-	Edge **unused_edges;
-	size_t idx;
-	size_t size;
+    Edge *edges;
+    Edge **unused_edges;
+    size_t idx;
+    size_t size;
 };
 
 /* Usefull macros
